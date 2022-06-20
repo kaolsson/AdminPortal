4d8f8a4d9168e9bf7840b44878df18ea59f84a38
kaolsson <ka_olsson@yahoo.com>
2022-06-20 06:48:40 -0700

    .

--- src/__fakeApi__/projectApi.js
+++ src/__fakeApi__/projectApi.js
@@ -35,14 +35,14 @@ class ProjectApi {
           }
         };
 
-//        const hello = 'hello';
-//        const formData2 = JSON.stringify(
-//            {
-//                hello
-//            }
-//          );
+        const hello = 'hello';
+        const formData2 = JSON.stringify(
+            {
+                hello
+            }
+          );
 
-        axios.post(apiUrl, formData, theHeaders)
+        axios.post(apiUrl, formData2, theHeaders)
           .then((response) => {
             resolve(response);
           })
