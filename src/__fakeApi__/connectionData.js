562cd2627c9c459533262b64cf5a292bf3f072b0
kaolsson <ka_olsson@yahoo.com>
2022-07-17 01:01:48 -0700

    .

--- src/__fakeApi__/connectionData.js
+++ src/__fakeApi__/connectionData.js
@@ -3,8 +3,8 @@
 //
 export const serverConnection = {
     // server base url -- NEED TO MAKE THIS AUTOMATIC
-//    baseUrl: 'http://localhost:6543',
-    baseUrl: 'https://api.mysmartmaster.com',
+    baseUrl: 'http://localhost:6543',
+//    baseUrl: 'https://api.mysmartmaster.com',
 
     slash: '/',
 
